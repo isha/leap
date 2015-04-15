9947a721ce29a2796d743a120d2c3ec66e01cb7b
Isha <isha.kumaarr@gmail.com>
2015-04-15 16:29:25 -0700

    tru gitignoring secret file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Leap::Application.config.secret_token = ENV['SECRET_TOKEN']
+Leap::Application.config.secret_token = '6f1351a20b1afe46b63c2ab79a8ee8e20ef7cff52e8a79c052dca00c686aa3b83e637756a120683ecff6844eea9b29d928bcf92c8a65605a96fe8d6ba0a8e4d9'
