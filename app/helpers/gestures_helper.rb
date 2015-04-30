9ac3cefcbbc50611cb8819f62336cac74ee0f642
Isha <isha.kumaarr@gmail.com>
2015-04-30 16:13:29 -0700

    Show last modified in the index as well

--- app/helpers/gestures_helper.rb
+++ app/helpers/gestures_helper.rb
@@ -1,6 +1,6 @@
 module GesturesHelper
-  def sortable(column)
-    title = column.titleize
+  def sortable(column, title = nil)
+    title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to "#{title} <i class='#{direction == "desc" ? "icon-chevron-down" : "icon-chevron-up"}'></i>".html_safe, params.merge(:sort => column, :direction => direction), {:class => css_class}
